e09e67b3e2219fb7c217a0125bab25373b4b7d60
Ill <126349149+Ill-27@users.noreply.github.com>
2026-04-15 22:02:42 +0300

    Update data.js

--- data.js
+++ data.js
@@ -14,13 +14,24 @@ const ARTANAT_DB = {
     // ---- ВЕТКИ (ПАПКИ НА САЙТЕ) ----
     branches: [
         { tab: "arterrii", id: "the_last_chalice", name: { ru: "Последняя Чаша", en: "The Last Chalice", zh: "最后一尊圣杯" }, password: "" },
-        { tab: "base", id: "serbian", name: { ru: "Сербский: 120 Песен", en: "Serbian: 120 Songs" }, password: "" },
-        { tab: "arterrii", id: "dark_secrets", name: { ru: "Тайные мысли", en: "Dark Secrets" }, password: "dante" },
-        { tab: "friends", id: "italian", name: { ru: "Итальянский: Глаголы", en: "Italian: Verbs" }, password: "" }
+        { tab: "feed", id: "feed", name: "", password: "" }
     ],
 
     // ---- ТВОИ СООБЩЕНИЯ ----
     posts: [
+        {
+            branchId: "feed",
+            author: "Dante_Sogliano",
+            date: "15.04.2026 21:35",
+            // Для перевода пиши текст так:
+            text: {
+                en: "Sometimes I think, what is this all for... why do something so dangerous, meaningless, and difficult, but then I remember that out of everything, I love only you, and this is the only way to be with you.",
+                es: "A veces pienso para qué es esto... por qué hacer algo tan peligroso, sin sentido y difícil, pero luego recuerdo que, de todo, solo te amo a ti y esta es la única forma de estar contigo.",
+                ru: "Иногда я думаю, зачем это... зачем делать что-то столь опасное, бессмысленное и тяжёлое, но потом вспоминаю, что из всего люблю лишь тебя и это единственный способ быть с тобой.",
+                it: "A volte mi chiedo a che scopo... perché fare qualcosa di così pericoloso, insensato e difficile, ma poi ricordo che tra tutto amo solo te, e questo è l'unico modo per stare con te.",
+                sr: "Ponekad mislim, čemu sve ovo... zašto raditi nešto tako opasno, besmisleno i teško, ali onda se setim da od svega volim samo tebe i to je jedini način da budem s tobom.",
+                zh: "有时候我会想，这是为了什么……为什么要去做如此危险、毫无意义又艰难的事，但随后我就会想起，在这一切之中我只爱你，而这是能和你在一起的唯一方法。"
+        },
         {
             branchId: "the_last_chalice",
             author: "Dante_Sogliano",
@@ -34,17 +45,6 @@ const ARTANAT_DB = {
             },
             mediaUrl: "media/arterrii/the_last_chalice/VID_20260413_073310_438.mp4",
             mediaType: "video"
-        },
-        {
-            branchId: "my_story",
-            author: "Dante_Sogliano",
-            date: "15.04.2026 14:00",
-            // Если перевода нет, просто оставь русский, сайт покажет его по умолчанию
-            text: {
-                ru: "Звезды сегодня особенно яркие."
-            },
-            mediaUrl: "media/arterrii/image1.jpg", 
-            mediaType: "image"
         }
     ]
 };
