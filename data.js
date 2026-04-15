a5d88b2c74175eda20676c4709f158661c9f8c9d
Ill <126349149+Ill-27@users.noreply.github.com>
2026-04-15 14:01:58 +0300

    Update data.js

--- data.js
+++ data.js
@@ -9,28 +9,27 @@ const ARTANAT_DB = {
         { tab: "arterrii", id: "the_last_chalice", name: "Последняя Чаша", password: "" },
         { tab: "base", id: "serbian", name: "Сербский: 120 Песен", password: "" },
         { tab: "arterrii", id: "dark_secrets", name: "Тайные мысли", password: "dante" },
-        { tab: "friends", id: "italian", name: "Итальянский: Глаголы", password: "" },
+        { tab: "friends", id: "italian", name: "Итальянский: Глаголы", password: "" }
     ],
 
     // ---- ТВОИ СООБЩЕНИЯ ----
     // Новые сообщения добавляй ВЕРХ списка
     posts: [
         {
-        branchId: "the_last_chalice",
-        author: "Dante_Sogliano",
-        date: "15.04.2026 13:48",
-        text: "Последняя чаша...",
-        mediaUrl: "media/arterrii/the_last_chalice/VID_20260413_073310_438.mp4", // Путь к файлу в твоих папках
-        mediaType: "video"
-    },
+            branchId: "the_last_chalice", // <- Исправлено: точное совпадение с id ветки
+            author: "Dante_Sogliano",
+            date: "15.04.2026 13:48",
+            text: "Последняя чаша...",
+            mediaUrl: "media/arterrii/the_last_chalice/VID_20260413_073310_438.mp4", 
+            mediaType: "video"
+        },
         {
             branchId: "my_story",
             author: "Dante_Sogliano",
             date: "15.04.2026 14:00",
             text: "Первая картинка из папки media. Все работает идеально.",
-            // Путь к файлу: папка media -> папка arterrii -> файл image1.jpg
             mediaUrl: "media/arterrii/image1.jpg", 
-            mediaType: "image" // "image", "video" или "audio"
-        },
+            mediaType: "image"
+        }
     ]
 };
