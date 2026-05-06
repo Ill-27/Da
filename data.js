b73b3426724a141e8f2a6b5ed76a8febc7d06dc9
Ill <126349149+Ill-27@users.noreply.github.com>
2026-05-06 16:31:17 +0300

    Update data.js

--- data.js
+++ data.js
@@ -21,6 +21,15 @@ const ARTANAT_DB = {
     // ---- ТВОИ СООБЩЕНИЯ ----
     posts: [
         {
+            id: "post_011",
+            branchId: "feed",
+            author: "Dante_Sogliano",
+            date: "06.05.2026 16:49",
+            text: { en: "" }, 
+            mediaUrl: "media/Screenshot_20260506_162640_Video Player.jpg", 
+            mediaType: "image"
+        },
+        {
             id: "post_010",
             branchId: "feed",
             author: "Dante_Sogliano",
@@ -111,8 +120,6 @@ Pero no hay sentido en playas desiertas.
 
 A veces pienso para qué es esto... por qué hacer algo tan peligroсо, sin sentido y difícil, pero luego recuerdo que, de todo, solo te amo a ti y esta es la única forma de estar contigo.
 
-Иногда я думаю, зачем это... зачем делать что-то столь опасное, бессмысленное и тяжёлое, но потом вспоминаю, что из всего люблю лишь тебя и это единственный способ быть с тобой.
-
 Ponekad mislim, čemu sve ovo... zašto raditi nešto tako opasno, besmisleno i teško, ali onda se setim da od svega volim samo tebe i to je jedini način da budem s tobom.
 
 有时候我会想，这是为了什么……为什么要去做如此危险、毫无意义又艰难的事，但随后我就会想起，在这一切之中我只爱你，而这是能和你在一起의唯一方法。`
@@ -138,3 +145,4 @@ Ponekad mislim, čemu sve ovo... zašto raditi nešto tako opasno, besmisleno i 
          }
     ]
 };
+ 
